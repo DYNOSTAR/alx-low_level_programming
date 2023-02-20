2f98a5587d51da1496fc4cdade01d121431fff99
DYNOSTAR <joshuamuhorondirangu@gmail.com>
2023-02-20 05:27:01 -0800

    code

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,15 +7,15 @@
 int main(void)
 {
 	int num;
-	char n;
+	char la;
 
 	for (num = 0; num < 10; num++)
 		putchar((num % 10) + '0');
 
-	for (n = 'a'; n <= 'f'; n++)
-		putchar(n);
+	for (la = 'a'; la <= 'f'; la++)
+		putchar(la);
 
-	putchar('\n')
+	putchar('\n');
 
 	return (0);
 }
