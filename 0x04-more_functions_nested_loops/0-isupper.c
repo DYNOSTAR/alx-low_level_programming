8aa3187bcdf7764bfe03eeaa506b0f7e4a083008
DYNOSTAR <joshuamuhorondirangu@gmail.com>
2023-02-23 18:43:48 +0300

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
-* _isdigit - funtion to check for a digit
-* @c: an integer argument
-* Return: success
+* _isupper -> this is a function to check upper case alphas
+* @c: an integer paramer
+* Return: something but success
 */
-int _isdigit(int c)
+int _isupper(int c)
 {
-	return (c >= '0' && c <= '9');
+	return (c >= 'A' && c <= 'Z');
 }
